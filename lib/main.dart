9ed8b7d47115332e05fed418bce3c339e82f8a34
oovikat <mezet30@hotmail.com>
2023-05-09 13:03:55 +0200

    Firebase data

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
 
 void main() {
   runApp(MyApp());
